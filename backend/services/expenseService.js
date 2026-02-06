df4cc9a8454421e1addba3931eacf24182c358d1
Arpita0909-prog <manudwivedi044@gmail.com>
2026-02-06 09:20:56 -0800

    adding transaction to avoid unwanted changes in database

--- backend/services/expenseService.js
+++ backend/services/expenseService.js
@@ -1,17 +1,27 @@
 const Expense = require("../models/expenseModel");
 const User = require("../models/userModel");
+const sequelize = require("../utils/database");
 
 exports.addExpense = async ({ amount, description, category, userId }) => {
-    await User.increment('totalExpense', {
-        by: amount,
-        where: { id: userId }
-    });
-    return await Expense.create({
-        amount,
-        description,
-        category,
-        UserId: userId
-    });
+    const t = await sequelize.transaction();
+    try {
+        await User.increment('totalExpense', {
+            by: amount,
+            where: { id: userId },
+            transaction: t
+        });
+        const expense = await Expense.create({
+            amount,
+            description,
+            category,
+            UserId: userId
+        }, { transaction: t });
+        await t.commit();
+        return expense;
+    } catch (error) {
+        await t.rollback();
+        throw error;
+    }
 };
 
 exports.getExpenses = async (userId) => {
@@ -21,20 +31,30 @@ exports.getExpenses = async (userId) => {
 };
 
 exports.deleteExpense = async (expenseId, userId) => {
-    const expense = await Expense.findOne({
-        where: {
-            id: expenseId,
-            UserId: userId
+    const t = await sequelize.transaction();
+    try {
+        const expense = await Expense.findOne({
+            where: {
+                id: expenseId,
+                UserId: userId
+            },
+            transaction: t
+        });
+
+        if (!expense) {
+            await t.rollback();
+            throw new Error("NOT_ALLOWED");
         }
-    });
 
-    if (!expense) {
-        throw new Error("NOT_ALLOWED");
+        await User.decrement('totalExpense', {
+            by: expense.amount,
+            where: { id: userId },
+            transaction: t
+        });
+        await expense.destroy({ transaction: t });
+        await t.commit();
+    } catch (error) {
+        await t.rollback();
+        throw error;
     }
-
-    await User.decrement('totalExpense', {
-        by: expense.amount,
-        where: { id: userId }
-    });
-    await expense.destroy();
 };
